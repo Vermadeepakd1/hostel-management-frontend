ca1341d008107db2488ab00e56b2a9b0c90ae72d
FasterThanAi <demeanor404@gmail.com>
2025-10-06 16:09:30 +0530

    fix: comment out 'Other' gender option in StudentForm

--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -61,7 +61,7 @@ function StudentForm({ student, onSave, onCancel, setStudent }) {
             <option value="">Select Gender</option>
             <option value="Male">Male</option>
             <option value="Female">Female</option>
-            <option value="Other">Other</option>
+            {/* <option value="Other">Other</option> */}
           </select>
         </div>
 
